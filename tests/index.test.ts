6fdfa163a443f0dc1383ba185089c81b105080de
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-12-25 23:46:30 -0500

    test: setup operations json

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,10 +2,24 @@ function typed<T>(value: T): T {
 	return value;
 }
 
+function updateArray<T>(array: T[], index: number, value: T) {
+	return array.map((item, i) => i === index ? value : item);
+}
+
 const database = {
 	x: typed<number[]>([]),
 	y: typed<number[]>([]),
 };
+
+const operations = {
+	createX: (value: number) => database.x = [...database.x, value],
+	updateX: (index: number, value: number) => database.x = updateArray(database.x, index, value),
+	deleteX: (index: number) => database.x = database.x.filter((_, i) => i !== index),
+
+	createY: (value: number) => database.y = [...database.y, value],
+	updateY: (index: number, value: number) => database.y = updateArray(database.y, index, value),
+	deleteY: (index: number) => database.y = database.y.filter((_, i) => i !== index),
+}
 
 interface Permissions {
 	admin: boolean;
